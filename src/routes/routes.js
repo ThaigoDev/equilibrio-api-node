9f405a5dfa7b17ab6351806b2ae70c73213ce510
ThaigoDev <thaigodeveloper@gmail.com>
2025-04-23 09:46:28 -0300

    feat/login-feature-foi feito a funcionalidade de login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
 const router = express.Router();  
 const HomeController = require("../Controllers/HomeController")
+const LoginAndSignUpController = require("../Controllers/Login&SignUpController")
 
-router.get("/", HomeController.index) 
+router.get("/", HomeController.index)  
+router.post("/signup", LoginAndSignUpController.createAccount);
+router.post("/login", LoginAndSignUpController.login);
 
 
 module.exports = router

8d458a06b937d7c75edd79dfd91282511767ce4e
ThaigoDev <thaigodeveloper@gmail.com>
2025-06-16 08:56:02 -0300

    att

--- src/Models/SettingsModel.js
+++ src/Models/SettingsModel.js
@@ -1,9 +1,9 @@
 const mongoose  = require('mongoose')  
 const GoalSchema = require('../Models/GoalsModel')
 const SettingsSchema = new mongoose.Schema({
+   goals: GoalSchema,
    notifyEnabled: { type: Boolean, default: false },
    notifyHour: { type: String, default: "20:00" },
-   goals: GoalSchema,
  }, { _id: false }); 
 
  
